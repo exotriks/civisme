564730c604a9a8172afc48f0a58108766a7905d5
exotriks <civismebelgique@gmail.com>
2022-12-16 18:42:53 +0100

    first commit

---
+++ civisme/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from civisme.users.models import User
+from civisme.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
